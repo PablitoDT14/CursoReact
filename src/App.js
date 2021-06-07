2a9f41b8e592b6d8da46b7ff7d966f21887e47f7
PablitoDT14 <pablo.eze.aguirre@gmail.com>
2021-06-07 01:36:53 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer'
+import Children from "./components/Children"
 
 function App() {
   return (
     <div>
       <NavBar />
       <ItemListContainer greeting="Bienvenido al portal de compras" />
+      <Children>
+        <h1>Hola React</h1>
+      </Children>
     </div>
 
   );
--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Component } from "react"
 import Carrito from "./carrito.png"
 
 function CartWidget() {
