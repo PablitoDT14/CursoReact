dc9a78e01eae590f02eeea257c3fa9cac205f734
@A307349 <paguirre@santandertecnologia.com.ar>
2021-06-24 15:58:25 -0300

    mas diseño

--- src/components/Item.js
+++ src/components/Item.js
@@ -19,16 +19,15 @@ function Item() {
           return (
             <div key={producto.id}>
               <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={producto.image} />
+                <Card.Img variant="top" style={{'width':'300px', 'height':'300px', 'object-fit':'scale-down'}} src={producto.image} />
                 <Card.Body>
                   <Card.Title>{producto.title}</Card.Title>
-                  <Card.Text>{producto.description}</Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                  <ListGroupItem>${producto.price}</ListGroupItem>
+                  <ListGroupItem className="precio">${producto.price}</ListGroupItem>
                 </ListGroup>
-                <Card.Body>
-                  <Link to={`/products/${producto.id}`}>Ver Item</Link>
+                <Card.Body className="details">
+                  <Link  to={`/products/${producto.id}`}>Detalles</Link>
                 </Card.Body>
               </Card>
             </div>
