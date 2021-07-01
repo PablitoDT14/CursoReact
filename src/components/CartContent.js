9f8b394ebdcde8d9dc4ae750bc560be0c3e954e6
PablitoDT14 <pablo.eze.aguirre@gmail.com>
2021-07-01 10:21:29 -0300

    masProdsalCart

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -5,25 +5,26 @@ import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 function CartContent() {
     const {cart} = UseCart()
     if(cart.length>0){
-        console.log(cart)
-        for(let i=0;i<=cart.length;i++) { 
-          console.log(i)
-          return (
-              <div key={cart[i].id}>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" style={{'width':'100px', 'height':'100px', 'objectFit':'scale-down'}} src={cart[i].image} alt='   Imagen no disponible' />
-                  <Card.Title>{cart[i].product}</Card.Title>
-                  <Card.Body>
-                    <ListGroupItem className="precio">Cantidad: {cart[i].cantidad}</ListGroupItem>
-                    <ListGroupItem className="precio">Precio unitario: ${cart[i].precio}</ListGroupItem>
-                  </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem className="precio">Total: ${cart[i].total}</ListGroupItem>
-                  </ListGroup>
-                </Card>
-              </div>
-            )
-    }}
+        const carro= cart.map((item, index)=>{
+        return(
+        <Card style={{ width: '18rem' }}>
+                       <Card.Img variant="top" style={{'width':'100px', 'height':'100px', 'objectFit':'scale-down'}} src={item.image} alt='   Imagen no disponible' />
+                       <Card.Title>{item.product}</Card.Title>
+                       <Card.Body>
+                         <ListGroupItem className="precio">Cantidad: {item.cantidad}</ListGroupItem>
+                         <ListGroupItem className="precio">Precio unitario: ${item.precio}</ListGroupItem>
+                       </Card.Body>
+                       <ListGroup className="list-group-flush">
+                         <ListGroupItem className="precio">Total: ${item.total}</ListGroupItem>
+                       </ListGroup>
+                     </Card>
+        )})
+    return(
+      <div>
+        {carro}
+      </div>
+    )
+    }
     else{
         return (
             <div>
