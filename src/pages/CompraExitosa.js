5c7143bc63e7bc12938d2476a74b0c6c2f0a5f3b
PablitoDT14 <pablo.eze.aguirre@gmail.com>
2021-07-11 23:28:20 -0300

    Update CompraExitosa.js

--- src/pages/CompraExitosa.js
+++ src/pages/CompraExitosa.js
@@ -3,7 +3,9 @@ import React from 'react'
 function CompraExistosa() {
     return (
         <div>
-            GRACIAS POR SU COMPRA!
+            <h1>GRACIAS POR SU COMPRA!</h1>
+            <h2>En instantes estará recibiendo en su correo electrónico la confirmación del pedido</h2>
+            
         </div>
     )
 }
